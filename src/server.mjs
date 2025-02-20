a800a4cc43ca00b654bbdb5d6924fd0a5a0290c1
Johan Fernando Aponte Valencia <wdjohanwd@gmail.com>
2025-02-20 09:28:26 +0100

    instalacion mongo

--- src/server.mjs
+++ src/server.mjs
@@ -22,15 +22,13 @@ app.use(upload.single('archivo'));
 //elegimos puerto
 app.set('port', process.env.PORT || 5000);
 //peticiones
-app.get('/', (req,res) =>{
+app.get('/', (req, res) => {
     res.send('Servidor corriendo');
 });
 
 
-server.listen(app.get('port'),() =>{
+server.listen(app.get('port'), () => {
     console.log(`Servidor en marcha en el puerto `, app.get('port'));
 })
-//mongoose.connect('mongodb://admin:abc123@localhost:27017/bbdd?authSource=admin')
-mongoose.connect('mongodb://root:renaido@localhost:27017/bbdd?authSource=admin')
-    .then(db=>console.log('conectado a mongodb'))
-    .catch(err=>console.log(err));  
+//mongoose.connect('mongodb://root:renaido@localhost:27017/bbdd?authSource=admin')
+mongoose.connect('mongodb://admin:abc123@localhost:27017/bbdd?authSource=admin').then(db => console.log('conectado a mongodb')).catch(err => console.log(err));  
