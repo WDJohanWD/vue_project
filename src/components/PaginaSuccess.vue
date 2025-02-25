80aaed4200d848ffbccc85114c527416538dfb87
Johan Fernando Aponte Valencia <wdjohanwd@gmail.com>
2025-02-25 11:28:32 +0100

    success

---
+++ src/components/PaginaSuccess.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="payment-success">
+        <h1>Pago Completado</h1>
+        <p>¡Gracias por tu compra! Tu pago ha sido procesado exitosamente.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PaginaSucess'
+}
+</script>
+
+<style scoped>
+.payment-success {
+    text-align: center;
+    margin-top: 50px;
+}
+
+.payment-success h1 {
+    color: #4CAF50;
+}
+
+.payment-success p {
+    font-size: 18px;
+}
+</style>
