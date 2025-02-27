2c06e41d4c01466a08ccd9e39f666f5003154e3c
Johan Fernando Aponte Valencia <wdjohanwd@gmail.com>
2025-02-27 09:22:58 +0100

    arreglado contacto

--- src/router/rutas.mjs
+++ src/router/rutas.mjs
@@ -5,6 +5,7 @@ import path from 'path';
 import multer from 'multer';
 import Stripe from 'stripe';
 import 'dotenv/config';
+import nodemailer from 'nodemailer';
 
 console.log(Articulo)
 
@@ -205,5 +206,41 @@ rutas.post('/crear-checkout-session', async (req, res) => {
     }
 });
 
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false,
+    }
+});
+
+console.log('EMAIL_USER:', process.env.EMAIL_USER);
+console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
+
+rutas.post('/enviar-correo', (req, res) => {
+    console.log("Datos Recibidos: ", req.body);
+    const { nombre, telefono, email, mensaje } = req.body;
+
+    const mailOptions = {
+        from: email,
+        to: 'wdjohanwd@gmail.com', // aquí pones una dirección existente para probar
+        subject: 'Mensaje de contacto',
+        text: `Nombre: ${nombre}\nTeléfono: ${telefono}\nEmail: ${email}\nMensaje: ${mensaje}`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error("Error al enviar el correo:", error);
+            return res.status(500).json({ error: 'Error al enviar el mensaje, por favor inténtelo nuevamente.' });
+        } else {
+            console.log('Email enviado: ' + info.response);
+            return res.status(200).json({ message: 'Mensaje enviado correctamente' });
+        }
+    });
+});
 
 export default rutas;
